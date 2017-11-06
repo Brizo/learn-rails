a4a49b1141658d6565d7b5399fe803e5e3590c42
Brian Sihlongonyane <bryannhlakes@gmail.com>
2017-11-06 12:38:53 +0200

    Logs

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,6 +8,7 @@ class Owner
   end
 
   def countdown
+  	Rails.logger.info 'INFO: entering Owner countdown method'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
 	if birthday > today
@@ -16,5 +17,5 @@ class Owner
 	  countdown = (birthday.next_year - today).to_i
 	end
   end
-  
+
 end
